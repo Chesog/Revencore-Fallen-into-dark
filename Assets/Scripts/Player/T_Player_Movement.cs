4d73c3952560a9c4ed313df18b6b6c4fa9744e2d
DarioOtr <otrantodario@gmail.com>
2023-08-13 19:44:15 -0300

    Add Test Scene and Basic Player Movement

---
+++ Assets/Scripts/Player/T_Player_Movement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T_Player_Movement : MonoBehaviour
+{
+    [SerializeField] Vector3 movement = Vector3.zero;
+    [SerializeField] Rigidbody rigidbody;
+    [SerializeField] float speed;
+    [SerializeField] float z_speed;
+    [SerializeField] float jumpForce;
+    void Update()
+    {
+        z_speed = speed / 2;
+
+        movement.x = Input.GetAxisRaw("Horizontal") * z_speed;
+        movement.z = Input.GetAxisRaw("Vertical") * speed;
+
+        if (Input.GetKey(KeyCode.D)) 
+        {
+            transform.forward = Vector3.right;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.forward = Vector3.left;
+        }
+
+        movement.y = rigidbody.velocity.y;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        rigidbody.velocity = movement;
+    }
+}
