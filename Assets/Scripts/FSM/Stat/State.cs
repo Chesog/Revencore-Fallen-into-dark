88e3008f234a126fcc73b2db8bc7a2b08d0cbc31
Cheso <otrantodario@gmail.com>
2023-09-04 09:43:05 -0300

    Add new Input System

---
+++ Assets/Scripts/FSM/Stat/State.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditorInternal;
+
+public enum CharacterState
+{
+    IDLE,
+    WALKING,
+    RUNNING,
+    JUMPING,
+    ATTACKING,
+    TAKINGDAMAGE,
+    DEAD
+}
+
+/// <summary>
+/// Base Class For The States That We Create
+/// </summary>
+public class State
+{
+    public string name;
+    protected State_Machine state_Machine;
+    protected Dictionary<string, State> transitions = new();
+
+    public State(string name, State_Machine state_Machine)
+    {
+        this.name = name;
+        this.state_Machine = state_Machine;
+    }
+
+    /// <summary>
+    /// This Is Called When We Enter A State
+    /// </summary>
+    public virtual void OnEnter() { }
+
+    /// <summary>
+    /// Fuction To Update The Logic of A State (Update)
+    /// </summary>
+    public virtual void UpdateLogic() { }
+
+    /// <summary>
+    /// Fuction To Update The Pgysics of A State (Fixed Update)
+    /// </summary>
+    public virtual void UpdatePhysics() { }
+
+    /// <summary>
+    /// This Is Called When We Exit A State
+    /// </summary>
+    public virtual void OnExit() { }
+
+    /// <summary>
+    ///  Function To Add The Posible Transitions For A State
+    /// </summary>
+    /// <param name="transitionName"></param>
+    /// <param name="transitionState"></param>
+    public virtual void AddStateTransitions(string transitionName, State transitionState)
+    {
+        transitions.Add(transitionName, transitionState);
+    }
+}
---
+++ Assets/Scripts/FSM/StateMachine/State_Machine.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Base Class For The State Machine That We Create
+/// </summary>
+public class State_Machine : MonoBehaviour
+{
+    [SerializeField] public State currentState;
+    [HideInInspector]
+    public UnityEvent OnStateEnter;
+    [HideInInspector]
+    public UnityEvent OnStateExit;
+
+    protected virtual void OnEnable()
+    {
+        currentState = GetInitialState();
+        if (currentState != null)
+            currentState.OnEnter();
+    }
+
+    private void Update()
+    {
+        if (currentState != null)
+            currentState.UpdateLogic();
+    }
+
+    private void FixedUpdate()
+    {
+        if (currentState != null)
+            currentState.UpdatePhysics();
+    }
+
+    /// <summary>
+    /// Get The Initial State of The State Machine
+    /// </summary>
+    /// <returns></returns>
+    protected virtual State GetInitialState()
+    {
+        return null;
+    }
+
+    /// <summary>
+    /// Set a New State For the State Machine
+    /// </summary>
+    /// <param name="newState"></param>
+    public void SetState(State newState)
+    {
+        if (newState == null)
+        {
+            Debug.LogError($"{name} : New State Is Null", this);
+            return;
+        }
+
+        if (newState == currentState)
+            return;
+
+        OnStateExit?.Invoke();
+        currentState.OnExit();
+
+        currentState = newState;
+
+        OnStateEnter?.Invoke();
+        currentState.OnEnter();
+
+        Debug.Log($"Current State : {currentState.name}");
+    }
+}
