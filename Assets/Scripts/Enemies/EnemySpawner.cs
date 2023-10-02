352d1663a6e525874ccfe2a8ae26bb45980d4c51
gytsss <totogodoy@hotmail.com>
2023-10-02 12:37:00 -0300

    Add enemies spawner

---
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemySpawner : MonoBehaviour
+{
+    #region EXPOSED_FIELDS
+
+    [SerializeField] private GameObject _meleeEnemyPrefab;
+    [SerializeField] private GameObject _distanceEnemyPrefab;
+
+    [SerializeField] private float _meleeInterval = 3.5f;
+    [SerializeField] private float _distanceInterval = 10f;
+    [SerializeField] private int _maxMeleeEnemies = 5;
+    [SerializeField] private int _maxDistanceEnemies = 5;
+
+    #endregion
+
+    #region PRIVATE_FIELDS
+
+    private int _count = 0;
+
+    #endregion
+
+    #region UNITY_CALLS
+
+    private void Start()
+    {
+        StartCoroutine(spawnEnemy(_meleeInterval, _meleeEnemyPrefab, _maxMeleeEnemies));
+    }
+
+    private IEnumerator spawnEnemy(float interval, GameObject enemy, int maxEnemies)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, transform.position, Quaternion.identity);
+        _count++;
+        if (_count < maxEnemies)
+            StartCoroutine(spawnEnemy(interval, enemy, maxEnemies));
+    }
+
+    #endregion
+}
