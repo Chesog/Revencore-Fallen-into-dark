aaeb6cf28e000ce0fb58a89d4d6c36edcaa2e389
gytsss <totogodoy@hotmail.com>
2023-10-30 23:04:10 -0300

    Build v0.11.1

--- Assets/Scripts/Knockback.cs
+++ Assets/Scripts/Knockback.cs
@@ -2,31 +2,46 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Knockback : MonoBehaviour
 {
+    #region EVENTS
+
     public event Action OnBeginKnockback, OnDoneKnockback;
-    
-    [SerializeField] private float strength = 16;
-    [SerializeField] private float delay = 0.15f;
+
+    #endregion
+
+    #region EXPOSED_FIELDS
+
+    [SerializeField] private float _strength = 16;
+    [SerializeField] private float _delay = 0.15f;
+
+    #endregion
+
+    #region PUBLIC_METHODS
 
     public void PlayKnockback(Transform sender)
     {
         StopAllCoroutines();
         OnBeginKnockback?.Invoke();
         Vector2 direction = (transform.position - sender.transform.position).normalized;
-        direction.y = 0; 
+        direction.y = 0;
         StartCoroutine(KnockbackCoroutine(direction));
     }
+
+    #endregion
+
+    #region PRIVATE_METHODS
 
     private IEnumerator KnockbackCoroutine(Vector2 direction)
     {
         float timer = 0;
         float originalZ = transform.position.z; // Save the original z value
 
-        while (timer < delay)
+        while (timer < _delay)
         {
-            Vector3 newPosition = (Vector2)transform.position + direction * (strength * Time.deltaTime);
+            Vector3 newPosition = (Vector2)transform.position + direction * (_strength * Time.deltaTime);
             newPosition.z = originalZ; // Restore the original z value
             transform.position = newPosition;
 
@@ -37,10 +52,5 @@ public class Knockback : MonoBehaviour
         OnDoneKnockback?.Invoke();
     }
 
-    // private IEnumerator Reset()
-    // {
-    //     yield return new WaitForSeconds(delay);
-    //     _rb.velocity = Vector3.zero;
-    //     OnDoneKnockback?.Invoke();
-    // }
+    #endregion
 }
