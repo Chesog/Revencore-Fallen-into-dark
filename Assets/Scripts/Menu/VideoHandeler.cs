f05b836cb2c0637e70644e4943a697375cf60752
Dario <otrantodario@gmail.com>
2023-11-21 22:24:48 -0300

    Add Stop Video Function

--- Assets/Scripts/Menu/VideoHandeler.cs
+++ Assets/Scripts/Menu/VideoHandeler.cs
@@ -1,14 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class VideoHandeler : MonoBehaviour
 {
     [SerializeField] private VideoPlayer _videoPlayer;
 
+    private void OnEnable()
+    {
+        StopVideo();
+    }
+
     public void PlayVideo()
     {
         _videoPlayer.Play();
+    }
+
+    public void StopVideo()
+    {
+        //gameObject.SetActive(false);
+        _videoPlayer.Stop();
+    }
+
+    private void OnDisable()
+    {
+        gameObject.SetActive(false);
+        StopVideo();
+    }
+
+    private void OnDestroy()
+    {
+        gameObject.SetActive(false);
+        StopVideo();
     }
 }
